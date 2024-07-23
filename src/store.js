5bcb6d3f79ca59aa5b5b6441b232ecd478b7c0d2
shankar1534 <114395979+shankar1534@users.noreply.github.com>
2024-07-23 21:16:03 +0530

    Update and rename redux to store.js
    new change

---
+++ src/store.js
@@ -0,0 +1,11 @@
+// src/app/store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
